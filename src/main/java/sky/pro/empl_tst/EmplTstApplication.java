f484aabea6b46750346fff6e6802a88e2dcf8a18
Александр <perchatki79@gmail.com>
2023-05-22 20:47:43 +0300

    Initial commit

---
+++ src/main/java/sky/pro/empl_tst/EmplTstApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.empl_tst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplTstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplTstApplication.class, args);
+	}
+
+}
